01286bf62a62a7f30686b76e5203cbef81e4a6e4
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-10-28 11:13:58 -0300

    :art: style(eslint): Enhance ESLint configuration with new rules and plugins for better code quality

--- node.js
+++ node.js
@@ -9,20 +9,75 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
   rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'no-empty-pattern': 'off',
+    'prefer-const': 'error',
     'prettier/prettier': [
       'error',
       {
         printWidth: 80,
-        tabWidth: 2,
+        tabWidth: 4,
         singleQuote: true,
         trailingComma: 'all',
+        quoteProps: 'as-needed',
         arrowParens: 'always',
-        semi: false,
+        semi: true,
+        endOfLine: 'auto',
+        useTabs: true,
+        jsxSingleQuote: false,
       },
     ],
   },
+  overrides: [
+    {
+      files: ['*.tsx', '*.ts'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // Imports com caracteres especiais, como imports de side effects
+              ['^\\u0000'],
+
+              // Imports dos frameworks principais, como React e Next.js
+              ['^react$', '^next'],
+
+              // Imports de pacotes de terceiros
+              ['^@?\\w', 'next-bricks'],
+
+              // Imports organizados por estrutura de projeto e pacotes de terceiros
+              [
+                // Subgrupo 1: Imports de módulos internos do projeto, como utils, components, etc.
+                '^(utils|hooks|types|contexts|components|middlewares|services|styles|assets|config|constants|controllers|helpers|icons|layouts|models|views|theme|themes)(/.*|$)',
+                // Subgrupo 3: Imports relativos (começando com './', '../', ou '.')
+                '^\\.',
+              ],
+
+              // Imports de estilização
+              ['^./styles'],
+
+              // Imports absolutos não cobertos pelos grupos anteriores
+              ['^[^.]'],
+
+              // Grupo geral de captura para qualquer coisa não capturada anteriormente
+              ['^'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
   settings: {
     'import/parsers': {
       [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
